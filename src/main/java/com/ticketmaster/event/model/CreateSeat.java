0c94ee845984b27f3dadfa515b410bd6a94782b4
Dev Patel <dev.patel@ticketmaster.com>
2018-06-22 13:22:28 -0700

    Creat Seat class created

---
+++ src/main/java/com/ticketmaster/event/model/CreateSeat.java
@@ -0,0 +1,29 @@
+package com.ticketmaster.event.model;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class CreateSeat {
+
+    public static void main( String[ ] args ) {
+
+        EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "org.hibernate.jpa.HibernatePersistenceProvider" );
+
+        EntityManager entitymanager = emfactory.createEntityManager();
+        entitymanager.getTransaction().begin();
+
+        Seat seat = new Seat();
+        seat.setAvailability(true);
+        seat.setAisle(false);
+        seat.setSeatType(0);
+
+
+        entitymanager.persist(seat);
+        entitymanager.getTransaction().commit();
+
+        entitymanager.close();
+        emfactory.close();
+    }
+
+}
--- src/main/java/com/ticketmaster/event/model/entityManagerFactory.java
+++
@@ -1,8 +0,0 @@
-package com.ticketmaster.event.model;
-
-public class entityManagerFactory {
-
-    public void statmanet(){
-        System.out.println("It worked");
-    }
-}

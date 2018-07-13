24540c42e1984bca474a299a0e61c1d4d637d484
Dev Patel <dev.patel@ticketmaster.com>
2018-07-13 15:53:02 -0700

    none

--- src/main/java/com/ticketmaster/event/repositories/SeatRepository.java
+++ src/main/java/com/ticketmaster/event/repositories/SeatRepository.java
@@ -11,12 +11,8 @@ import java.util.Optional;
 @Repository
 public interface SeatRepository extends JpaRepository<Seat, Long>
          {
-<<<<<<< HEAD
 
-     @Query(value = "SELECT count(s) FROM Seat s WHERE available =:available AND seatType =:seatType AND aisle=:aisle AND id =:id")
-=======
      @Query(value = "SELECT s FROM Seat s WHERE available =:available AND seatType =:seatType AND aisle=:aisle OR id =:id")
->>>>>>> addid
      ArrayList<Seat> findByAvailableAndSeatTypeAndAisleAndId(@Param("available")Boolean available,
                                                         @Param("seatType") Integer seatType,
                                                         @Param("aisle") Boolean aisle,

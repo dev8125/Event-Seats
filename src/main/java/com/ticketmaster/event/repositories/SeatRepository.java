f72667c292ca68ecea1e12ad40799bb3ca9dd44d
Dev Patel <dev.patel@ticketmaster.com>
2018-07-13 12:09:30 -0700

    adding seats to be loaded

--- src/main/java/com/ticketmaster/event/repositories/SeatRepository.java
+++ src/main/java/com/ticketmaster/event/repositories/SeatRepository.java
@@ -13,7 +13,7 @@ public interface SeatRepository extends JpaRepository<Seat, Long>
          {
              // "SELECT count(s) FROM Seat s WHERE available =:available AND seatType =:seatType AND aisle=:aisle AND id =:id"
 
-     @Query(value = "SELECT count(s) FROM Seat s WHERE available =:available AND seatType =:seatType AND aisle=:aisle AND id =:id")
+     @Query(value = "SELECT count(s) FROM Seat s WHERE available =:available AND seatType =:seatType AND aisle=:aisle OR id =:id")
      ArrayList<Seat> findByAvailableAndSeatTypeAndAisleAndId(@Param("available")Boolean available,
                                                         @Param("seatType") Integer seatType,
                                                         @Param("aisle") Boolean aisle,
